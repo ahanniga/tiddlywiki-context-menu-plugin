1045442d1c0dd4bbe8394739a6c771da3a864934
Aidan Hannigan <ahanniga@cisco.com>
2022-07-30 22:13:04 +0100

    Code reformat

--- src/$__plugins_ahanniga_context-menu_ContextListener.js
+++ src/$__plugins_ahanniga_context-menu_ContextListener.js
@@ -8,141 +8,141 @@ This widgets implements context menus to tiddlers
 
 (function () {
 
-  var sanitize = function (string) {
-    const map = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#x27;',
-        "/": '&#x2F;',
+    var sanitize = function (string) {
+        const map = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#x27;',
+            "/": '&#x2F;',
+        };
+        const reg = /[&<>"'/]/ig;
+        return string.replace(reg, (match) => (map[match]));
     };
-    const reg = /[&<>"'/]/ig;
-    return string.replace(reg, (match)=>(map[match]));
-  };
 
-  var getField = function(tid, field, dflt) {
-    if(!tid || !tid.fields || !tid.fields[field]) {
-      return dflt;
-    }
-    return tid.fields[field];
-  };
+    var getField = function (tid, field, dflt) {
+        if (!tid || !tid.fields || !tid.fields[field]) {
+            return dflt;
+        }
+        return tid.fields[field];
+    };
 
-  var htmlToElement = function (html) {
-    var template = document.createElement('template');
-    template.innerHTML = html.trim();
-    return template.content.firstChild;
-  }
-
-  var Widget = require("$:/core/modules/widgets/widget.js").widget;
-  var template = `<div id="contextMenu" class="context-menu" style="display: none; z-order: 9999;"></div>`;
-
-  var ContextListener = function (parseTreeNode, options) {
-    this.initialise(parseTreeNode, options);
-  };
-
-  ContextListener.prototype = new Widget();
-
-  ContextListener.prototype.render = function (parent, nextSibling) {
-    this.parentDomNode = parent;
-    var self = this;
-    parent.addEventListener("contextmenu", function (event) { self.contextmenu.call(self, event) });
-    document.onclick = this.hideMenu;
-  };
-
-  ContextListener.prototype.contextmenu = function (event) {
-    var self = this;
-    var menu = document.getElementById("contextMenu");
-
-    if(getSelection().toString().trim().length > 0) {
-      // User has selected text, so don't trigger this menu
-      return true;
+    var htmlToElement = function (html) {
+        var template = document.createElement('template');
+        template.innerHTML = html.trim();
+        return template.content.firstChild;
     }
 
-    if (menu == null) {
-      this.document.body.appendChild(htmlToElement(template));
-      menu = document.getElementById("contextMenu");
-      menu.addEventListener("click", function (event) { self.menuClicked.call(self, event) });
-    }
-    menu.innerHTML = "";
+    var Widget = require("$:/core/modules/widgets/widget.js").widget;
+    var template = `<div id="contextMenu" class="context-menu" style="display: none; z-order: 9999;"></div>`;
 
-    var menuHtml = ["<ul>"];
-    var titles = $tw.wiki.getTiddlersWithTag("$:/tags/tiddlercontextmenu");
-    var label, action, icon, tid, targ, text, separator;
+    var ContextListener = function (parseTreeNode, options) {
+        this.initialise(parseTreeNode, options);
+    };
 
-    for(var a = 0; a < titles.length; a++) {
-      tid = $tw.wiki.getTiddler(titles[a]);
-      text = sanitize(getField(tid, "text", "hide"));
+    ContextListener.prototype = new Widget();
 
-      if(text !== "show") {
-        continue;
-      }
+    ContextListener.prototype.render = function (parent, nextSibling) {
+        this.parentDomNode = parent;
+        var self = this;
+        parent.addEventListener("contextmenu", function (event) { self.contextmenu.call(self, event) });
+        document.onclick = this.hideMenu;
+    };
 
-      label = sanitize(getField(tid, "caption", "Unlabelled Option"));
-      action = sanitize(getField(tid, "tm-message", "tm-dummy"));
-      icon = $tw.wiki.getTiddlerText(getField(tid, "icon", "$:/core/images/blank"));
-      targ = event.currentTarget.getAttribute("data-tiddler-title");
-      separator = tid.fields["separate-after"] === undefined ? "" : "menu-separator";
-      menuHtml.push(`<li class="${separator}"><a action="${action}" targ="${targ}" href="#!">${icon} ${label}</a></li>`);
-    }
-    
-    menuHtml.push("</ul>");
-    menu.append(htmlToElement(menuHtml.join("")))
+    ContextListener.prototype.contextmenu = function (event) {
+        var self = this;
+        var menu = document.getElementById("contextMenu");
 
-    if (menu.style.display == "block") {
-      this.hideMenu();
-    } else {
-      menu.style.display = 'block';
-      menu.style.left = event.pageX + "px";
-      menu.style.top = event.pageY + "px";
-    }
+        if (getSelection().toString().trim().length > 0) {
+            // User has selected text, so don't trigger this menu
+            return true;
+        }
 
-    event.preventDefault();
-    return false;
-  };
+        if (menu == null) {
+            this.document.body.appendChild(htmlToElement(template));
+            menu = document.getElementById("contextMenu");
+            menu.addEventListener("click", function (event) { self.menuClicked.call(self, event) });
+        }
+        menu.innerHTML = "";
 
-  ContextListener.prototype.menuClicked = function (event) {
-    var action = event.target.getAttribute("action");
-    var targ = event.target.getAttribute("targ");
-    var tid, stid, state, text;
-    this.hideMenu();
+        var menuHtml = ["<ul>"];
+        var titles = $tw.wiki.getTiddlersWithTag("$:/tags/tiddlercontextmenu");
+        var label, action, icon, tid, targ, text, separator;
 
-    switch (action) {
-      case "tm-fold-tiddler":
-        stid = `$:/state/folded/${targ}`;
-        state = $tw.wiki.getTiddlerText(stid, "show") === "show" ? "hide" : "show";
-        $tw.wiki.setText(stid, "text", null, state);
-        break;
-      case "tm-copy-to-clipboard":
-        text = $tw.wiki.getTiddlerText(targ);
-        this.dispatchEvent({ type: action, param: text });
-        break;
-      case "tm-print":
-        this.dispatchEvent({ type: action, event: event });
-        break;
-      case "tm-unfold-all-tiddlers":
-        this.dispatchEvent({ type: action, param: targ, foldedStatePrefix: "$:/state/folded/" });
-        break;
-        
-      default:
-        this.dispatchEvent({ type: action, param: targ });
-    }
+        for (var a = 0; a < titles.length; a++) {
+            tid = $tw.wiki.getTiddler(titles[a]);
+            text = sanitize(getField(tid, "text", "hide"));
 
-    event.preventDefault();
-    return false;
-  };
+            if (text !== "show") {
+                continue;
+            }
 
-  ContextListener.prototype.refresh = function (changedTiddlers) {
-    return false;
-  };
+            label = sanitize(getField(tid, "caption", "Unlabelled Option"));
+            action = sanitize(getField(tid, "tm-message", "tm-dummy"));
+            icon = $tw.wiki.getTiddlerText(getField(tid, "icon", "$:/core/images/blank"));
+            targ = event.currentTarget.getAttribute("data-tiddler-title");
+            separator = tid.fields["separate-after"] === undefined ? "" : "menu-separator";
+            menuHtml.push(`<li class="${separator}"><a action="${action}" targ="${targ}" href="#!">${icon} ${label}</a></li>`);
+        }
 
-  ContextListener.prototype.hideMenu = function () {
-    var menu = document.getElementById("contextMenu");
-    if (menu != null) {
-      menu.style.display = "none";
-    }
-  };
+        menuHtml.push("</ul>");
+        menu.append(htmlToElement(menuHtml.join("")))
 
-  exports.contextMenu = ContextListener;
+        if (menu.style.display == "block") {
+            this.hideMenu();
+        } else {
+            menu.style.display = 'block';
+            menu.style.left = event.pageX + "px";
+            menu.style.top = event.pageY + "px";
+        }
+
+        event.preventDefault();
+        return false;
+    };
+
+    ContextListener.prototype.menuClicked = function (event) {
+        var action = event.target.getAttribute("action");
+        var targ = event.target.getAttribute("targ");
+        var tid, stid, state, text;
+        this.hideMenu();
+
+        switch (action) {
+            case "tm-fold-tiddler":
+                stid = `$:/state/folded/${targ}`;
+                state = $tw.wiki.getTiddlerText(stid, "show") === "show" ? "hide" : "show";
+                $tw.wiki.setText(stid, "text", null, state);
+                break;
+            case "tm-copy-to-clipboard":
+                text = $tw.wiki.getTiddlerText(targ);
+                this.dispatchEvent({ type: action, param: text });
+                break;
+            case "tm-print":
+                this.dispatchEvent({ type: action, event: event });
+                break;
+            case "tm-unfold-all-tiddlers":
+                this.dispatchEvent({ type: action, param: targ, foldedStatePrefix: "$:/state/folded/" });
+                break;
+
+            default:
+                this.dispatchEvent({ type: action, param: targ });
+        }
+
+        event.preventDefault();
+        return false;
+    };
+
+    ContextListener.prototype.refresh = function (changedTiddlers) {
+        return false;
+    };
+
+    ContextListener.prototype.hideMenu = function () {
+        var menu = document.getElementById("contextMenu");
+        if (menu != null) {
+            menu.style.display = "none";
+        }
+    };
+
+    exports.contextMenu = ContextListener;
 
 })();
